cfde1d2a0f0631315a6a134a83e3d9137f8c8f34
Satyajb <satyajyotibiswas@gmail.com>
2019-04-02 14:44:00 +0530

    Starter template Introduced

---
+++ js/canvas.js
@@ -0,0 +1,28 @@
+// first we need to create a stage
+const stage = new Konva.Stage({
+    container: 'container',   // id of container <div>
+    width: 500,
+    height: 500
+});
+  
+// then create layer
+const layer = new Konva.Layer();
+
+// create our shape
+const circle = new Konva.Circle({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4
+});
+
+// add the shape to the layer
+layer.add(circle);
+
+// add the layer to the stage
+stage.add(layer);
+
+// draw the image
+layer.draw();
