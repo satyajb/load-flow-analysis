3b257a7c0fca0e404bed195d73a8b23743c5e5de
Satyajb <satyajyotibiswas@gmail.com>
2019-04-24 14:40:04 +0530

    Manual SVG test successed

--- js/svg.js
+++ js/svg.js
@@ -1,41 +1,90 @@
 const svg = document.getElementById('my-svg');
 const svgns = 'http://www.w3.org/2000/svg';
 
-svg.addEventListener('click', startStop);
-svg.addEventListener('mousemove', drag);
+svg.addEventListener('mousedown', startDrawing);
+svg.addEventListener('mousemove', panDrawing);
+svg.addEventListener('mouseup', stopDrawing);
 
-let started = false,
-    line,
-    pos;
+// mode selector
+let currMode;
+function changeMode(e) {
+    currMode = e.options[e.selectedIndex].value;
+}
 
-function startStop(e) {
-    pos = getMousePosition(e);
-    if(!started) {
-        line = document.createElementNS(svgns, 'line');
-        line.setAttributeNS(null, 'x1', pos.x);
-        line.setAttributeNS(null, 'y1', pos.y);
-        line.setAttributeNS(null, 'x2', pos.x);
-        line.setAttributeNS(null, 'y2', pos.y);
-        line.setAttributeNS(null, 'stroke', '#3498db');
+const tool = document.getElementById('tool');
 
-        svg.appendChild(line);
-    } else {
-        line.setAttributeNS(null, 'x2', pos.x);
-        line.setAttributeNS(null, 'y2', pos.y);
-    }
-    started = !started;
+// line
+let started = false, startPos, endPos,
+    elem, mode;
+
+function startDrawing(ev) {
+    started = true;
+    startPos = getMousePosition(ev);
+
+    mode = tool.options[tool.selectedIndex].value;
+
+    elem = document.createElementNS(svgns, mode);
+    elem.setAttributeNS(null, 'stroke', '#3498db');
     
+    svg.appendChild(elem);
 }
 
-function drag(e) {
+function panDrawing(ev) {
     if(started) {
-        pos = getMousePosition(e);
-
-        line.setAttributeNS(null, 'x2', pos.x);
-        line.setAttributeNS(null, 'y2', pos.y);
+        endPos = getMousePosition(ev);
+        switch(mode) {
+            case 'line':
+                line(elem, startPos, endPos);
+            break;
+            
+            case 'rect':
+                rect(elem, startPos, endPos);
+            break;
+            
+            case 'circle':
+                circle(elem, startPos, endPos);
+            break;
+        }
     }
 }
 
+function stopDrawing(ev) {
+    started = false;
+}
+
+// line
+function line(elem, startPos, endPos) {
+    elem.setAttributeNS(null, 'x1', startPos.x);
+    elem.setAttributeNS(null, 'y1', startPos.y);
+    elem.setAttributeNS(null, 'x2', endPos.x);
+    elem.setAttributeNS(null, 'y2', endPos.y);
+}
+
+// rect
+function rect(elem, startPos, endPos) {
+    let a = Math.min(startPos.x, endPos.x),
+        b = Math.min(startPos.y, endPos.y),
+        c = Math.max(startPos.x, endPos.x),
+        d = Math.max(startPos.y, endPos.y);
+
+    elem.setAttributeNS(null, 'x', a);
+    elem.setAttributeNS(null, 'y', b);
+    
+    elem.setAttributeNS(null, 'width', c-a);
+    elem.setAttributeNS(null, 'height', d-b);
+}
+
+// circle
+function circle(elem, startPos, endPos) {
+    elem.setAttributeNS(null, 'cx', startPos.x);
+    elem.setAttributeNS(null, 'cy', startPos.y);
+
+
+    let r = Math.sqrt((startPos.x - endPos.x) ** 2 + (startPos.y - endPos.y) ** 2);
+    elem.setAttributeNS(null, 'r', r);
+}
+
+// utility 
 function getMousePosition(e) {
     return {
         x: e.clientX - svg.getBoundingClientRect().left,
