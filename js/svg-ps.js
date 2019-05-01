34b895dc8824777ada44a56711689e0b8d2d7b21
Satyajb <satyajyotibiswas@gmail.com>
2019-05-01 15:28:10 +0530

    Style updated

--- js/svg-ps.js
+++ js/svg-ps.js
@@ -1,125 +1,124 @@
 const svg = document.getElementById('my-svg');
-const svgns = 'http://www.w3.org/2000/svg';
-
-// svg.addEventListener('mousedown', startDrawing);
-// svg.addEventListener('mousemove', panDrawing);
-// svg.addEventListener('mouseup', stopDrawing);
-
-// mode selector
-let currMode;
-function changeMode(e) {
-    currMode = e.options[e.selectedIndex].value;
-}
-
+// const svgns = 'http://www.w3.org/2000/svg';
 const tool = document.getElementById('tool');
 
-// line
-let started = false, startPos, endPos,
-    elem, mode,
-    uniqId = 0;
+// Events
+let started = false;
+let line, bus;
 
-function startDrawing(ev) {
-    started = true;
-    startPos = getMousePosition(ev);
+svg.addEventListener('click', (ev) => {
+    let mp = getMousePosition(ev, svg),
+        mode = tool.options[tool.selectedIndex].value,
+        targetIsBus = ev.target.classList.contains('bus');
 
-    mode = tool.options[tool.selectedIndex].value;
+    if (mode == 'line') {
+        if (!started && targetIsBus) {
+            let _id = generateId();
+            line = new Line(_id, svg);
+            line.mp = mp;
+            line.start();
+            started = true;
+        } else if (started && !targetIsBus) {
+            line.mp = mp;
+            line.addPoint();
+        } else if (started && targetIsBus) {
+            line.mp = mp;
+            line.stop();
+            started = false;
 
-    elem = document.createElementNS(svgns, mode);
-    elem.setAttributeNS(null, 'stroke', '#3498db');
-    
-    svg.appendChild(elem);
-}
-
-function panDrawing(ev) {
-    if(started) {
-        endPos = getMousePosition(ev);
-        switch(mode) {
-            case 'line':
-                line(elem, startPos, endPos);
-            break;
-            
-            case 'rect':
-                rect(elem, startPos, endPos);
-            break;
-            
-            case 'circle':
-                circle(elem, startPos, endPos);
-            break;
+            // alert('line draw completed')
+        } else {
+            throw new Error('Line should start or terminate on a bus. If no bus is available, draw one first.');
         }
-    }
-}
-
-function stopDrawing(ev) {
-    started = false;
-}
-
-// line
-function line(elem, startPos, endPos) {
-    elem.setAttributeNS(null, 'x1', startPos.x);
-    elem.setAttributeNS(null, 'y1', startPos.y);
-    elem.setAttributeNS(null, 'x2', endPos.x);
-    elem.setAttributeNS(null, 'y2', endPos.y);
-    elem.setAttributeNS(null, 'stroke-width', 4);
-    elem.setAttributeNS(null, 'stroke-linecap', 'round');
-}
-
-// rect
-function rect(elem, startPos, endPos) {
-    let a = Math.min(startPos.x, endPos.x),
-        b = Math.min(startPos.y, endPos.y),
-        c = Math.max(startPos.x, endPos.x),
-        d = Math.max(startPos.y, endPos.y);
-
-    elem.setAttributeNS(null, 'x', a);
-    elem.setAttributeNS(null, 'y', b);
-    
-    elem.setAttributeNS(null, 'width', c-a);
-    elem.setAttributeNS(null, 'height', d-b);
-}
-
-// circle
-function circle(elem, startPos, endPos) {
-    elem.setAttributeNS(null, 'cx', startPos.x);
-    elem.setAttributeNS(null, 'cy', startPos.y);
-
-
-    let r = Math.sqrt((startPos.x - endPos.x) ** 2 + (startPos.y - endPos.y) ** 2);
-    elem.setAttributeNS(null, 'r', r);
-}
-
-svg,addEventListener('click', path);
-document.addEventListener('keypress', (ev) => {
-    if(ev.keyCode == 13) {
-        started = false;
-    }
-})
-
-
-
-
-let polyline, points = [];
-// path
-function path(ev) {
-    let mp = getMousePosition(ev);
-    
-    if(!started) {
-        polyline = document.createElementNS(svgns, 'polyline');
-        polyline.setAttributeNS(null, 'stroke', '#f25757');
-        polyline.setAttributeNS(null, 'stroke-width', '4');
-        polyline.setAttributeNS(null, 'stroke-linecap', 'round');
-        polyline.setAttributeNS(null, 'fill', 'none');
-
-        svg.appendChild(polyline);
-        started = true;
+    } else if (mode == 'bus') {
+        if (!targetIsBus) {
+            if (!started) {
+                let _id = generateId();
+                bus = new Bus(_id, svg);
+                bus.mp = mp;
+                bus.start();
+            } else {
+                bus.mp = mp;
+                bus.stop();
+            }
+            started = !started;
+        } else {
+            throw new Error('Buses should not start or terminate on another bus');
+        }
     } else {
-        points.push(mp.x, mp.y);
-        let ps = points.toString();
-        // console.log(ps)
-        polyline.setAttributeNS(null, 'points', ps);
+        throw new Error('No mode is selected');
+    }
+});
+
+
+
+// Constructor function
+// Line 
+function Line(id, pid) {
+    this.id = id;
+    this.parentId = pid;
+
+    this.mp = '';
+
+    let startPos, midPos = [], endPos;
+    let el = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+    el.setAttributeNS(null, 'stroke', '#3498db');
+    el.setAttributeNS(null, 'stroke-width', '3');
+    el.setAttributeNS(null, 'stroke-linecap', 'round');
+    el.setAttributeNS(null, 'fill', 'none');
+    el.setAttributeNS(null, 'class', 'line');
+    el.setAttributeNS(null, 'id', 'line' + this.id);
+
+    this.start = function () {
+        startPos = Object.values(this.mp);
+        el.setAttributeNS(null, 'points', startPos);
+        this.parentId.appendChild(el);
+    }
+
+    this.addPoint = function () {
+        midPos.push(Object.values(this.mp));
+        let _ps = startPos.concat(midPos);
+        el.setAttributeNS(null, 'points', _ps);
+    }
+
+    this.stop = function () {
+        endPos = Object.values(this.mp);
+        let _ps = startPos.concat(midPos, endPos);
+        el.setAttributeNS(null, 'points', _ps);
     }
 }
 
-// utility 
+// Bus
+function Bus(id, pid) {
+    this.id = id;
+    this.parentId = pid;
+
+    this.mp = '';
+
+    let startPos, endPos;
+    let el = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+    el.setAttributeNS(null, 'stroke', '#f25757');
+    el.setAttributeNS(null, 'stroke-width', '3');
+    el.setAttributeNS(null, 'stroke-linecap', 'round');
+    el.setAttributeNS(null, 'fill', 'none');
+    el.setAttributeNS(null, 'class', 'bus');
+    el.setAttributeNS(null, 'id', 'bus' + this.id);
+
+    this.start = function () {
+        startPos = Object.values(this.mp);
+        el.setAttributeNS(null, 'points', startPos);
+        this.parentId.appendChild(el);
+    }
+
+    this.stop = function () {
+        endPos = Object.values(this.mp);
+        let _ps = startPos.concat(endPos);
+        el.setAttributeNS(null, 'points', _ps);
+    }
+}
+
+
+// utility functions
 function getMousePosition(e) {
     return {
         x: e.clientX - svg.getBoundingClientRect().left,
@@ -127,11 +126,6 @@ function getMousePosition(e) {
     }
 }
 
-
-// Select svg elements
-// const btn = document.getElementById('my-btn');
-document.body.addEventListener('mouseover', (e) => {
-    if(e.target && e.target.tagName == 'line') {
-        console.log(e.target.tagName)
-    };
-});
+function generateId() {
+    return new Date().getTime();
+}
